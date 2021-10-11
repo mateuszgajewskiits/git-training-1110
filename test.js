46fde12fdbfc37db89aa87ec3aa827eb7d9fc5f3
Mateusz Gajewski <mateusz.gajewski@its-inter.pl>
2021-10-11 14:54:44 +0200

    Final commit

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-console.log("Hello World")
+console.log("Hello World");
 console.log("Hello World x2");
-console.log("Hello World x3")
+console.log("Hello World x3");
 console.log("Hello World x4");
