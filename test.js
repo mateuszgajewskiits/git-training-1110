7dfc7f2c1b0b22cea75f3fa76c4274da1cf8bc44
Mariusz.Serbinowski <mariusz.serbinowski@its-inter.pl>
2021-10-11 15:16:00 +0200

    mse changes

--- test.js
+++ test.js
@@ -1,2 +1,5 @@
-console.log("Hello World")
-console.log("Hello World x2")
+console.log("Hello World");
+console.log("Hello World x2");
+console.log("Herbata woda cukier");
+console.log("Herbata do woda wsadzic");
+console.log("Cukier do woda wsadzic");
